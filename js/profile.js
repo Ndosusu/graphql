cfe0c9a0372d273fa3ead90cf7464add6fe10413
ndossant <n.dossantos@outlook.fr>
2025-05-20 08:05:47 +0200

    unprofile

--- js/profile.js
+++ js/profile.js
@@ -6,24 +6,24 @@ document.getElementById("logout").addEventListener("click", () => {
   window.location.href = "index.html";
 });
 
-(async () => {
-  try {
-    const userData = await fetchGraphQL(`{
-      user {
-        id
-        login
-      }
-    }`);
+// (async () => {
+//   try {
+//     const userData = await fetchGraphQL(`{
+//       user {
+//         id
+//         login
+//       }
+//     }`);
 
-    document.getElementById("login").textContent = userData.user[0].login;
+//     document.getElementById("login").textContent = userData.user[0].login;
 
-    // fetchGraphQL(...) pour XP, résultats, etc.
+//     // fetchGraphQL(...) pour XP, résultats, etc.
 
-  } catch (err) {
-    alert("Session expirée ou erreur : " + err.message);
-    logoutUser();
-    window.location.href = "index.html";
-  }
-})();
+//   } catch (err) {
+//     alert("Session expirée ou erreur : " + err.message);
+//     logoutUser();
+//     window.location.href = "index.html";
+//   }
+// })();
 
 //debugger
